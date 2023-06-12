fdec4baa98320d12b85d65ccd8080b672be382f3
fcustodio90 <filipe.l.custodio@gmail.com>
2023-06-12 22:10:22 +0100

    Sidekiq setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require "sidekiq/web"
+Sidekiq::Web.app_url = "/"
+
 Rails.application.routes.draw do
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  mount Sidekiq::Web => "/sidekiq"
 end
